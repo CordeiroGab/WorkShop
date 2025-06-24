1909f580ade81800def426825f4e6582da180b09
Gabriel Neves <GabrielCordeiroDev@outlook.com>
2025-06-23 21:07:36 -0300

    banco de dados h2

--- src/main/java/org/projetospring/workshop/entities/User.java
+++ src/main/java/org/projetospring/workshop/entities/User.java
@@ -1,12 +1,18 @@
 package org.projetospring.workshop.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_usuarios")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
