9c35ebfcf32e2f265054b8ff745b920810474ab0
Gabriel Neves <GabrielCordeiroDev@outlook.com>
2025-07-01 18:25:24 -0300

    User insert

--- src/main/java/org/projetospring/workshop/resources/UserResources.java
+++ src/main/java/org/projetospring/workshop/resources/UserResources.java
@@ -7,11 +7,10 @@ import org.projetospring.workshop.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.PathMatcher;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,4 +37,13 @@ public class UserResources {
         User obj =  userServices.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userServices.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
 }
